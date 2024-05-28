c4706258684f307ed63434b132ecfc99732eede5
Ekin Koc <ekin@eknkc.com>
2024-05-28 23:03:06 +0300

    WIP

--- build.zig
+++ build.zig
@@ -24,7 +24,7 @@ pub fn build(b: *std.Build) void {
     });
 
     const lib = b.addStaticLibrary(.{
-        .name = "quickjs",
+        .name = "zquickjs",
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
